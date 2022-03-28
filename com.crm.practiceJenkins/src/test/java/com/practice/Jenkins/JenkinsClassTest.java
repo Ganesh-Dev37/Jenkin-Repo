d79662e627ccd212bcd140735b74fc6eb06a4957
USER <USER@HP>
2022-03-28 14:23:33 +0530

    Jenkins 1 commit

---
+++ com.crm.practiceJenkins/src/test/java/com/practice/Jenkins/JenkinsClassTest.java
@@ -0,0 +1,12 @@
+package com.practice.Jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsClassTest {
+	@Test
+	public void jenkin()
+	{
+		System.out.println("Jenkins Class Created");
+	}
+
+}
